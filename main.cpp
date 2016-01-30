f1c49236ca3a44e5d2b16ce576385150e9f1ee15
Allen <wildman279@gmail.com>
2016-01-30 01:28:50 -0500

    interactive serial control for RN52

--- main.cpp
+++ main.cpp
@@ -2,18 +2,47 @@
 
 Serial serial(p13, p14);
 Serial pc(USBTX, USBRX);
+DigitalOut cmdpin(p16);
+
+#define BUF_LEN 128
+char buf[BUF_LEN];
+char c;
+int  i;
 
 int main()
 {
     pc.baud(115200);
     serial.baud(115200);
+    cmdpin = 0;
+    i = 0;
 
-    pc.printf("\n\nMBED SERIAL TUNNEL\n");
+    pc.puts("\n\nMBED SERIAL TUNNEL\n");
 
     for (;;)
     {
         while (pc.readable())
-            serial.putc(pc.getc());
+        {
+            c = pc.getc();
+            buf[i++] = c;
+            if (c == '\r')
+            {
+                //buf[i] = '\n';
+                //buf[i+1] = '\0';
+                buf[i] = '\0';
+
+                //cmdpin = 0;
+                serial.puts(buf);
+                //cmdpin = 1;
+
+                pc.putc('\n');
+                i = 0;
+            }
+            else
+            {
+                pc.putc(c);
+            }
+        }
+
         while (serial.readable())
             pc.putc(serial.getc());
     }
